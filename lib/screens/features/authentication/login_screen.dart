558848858be3ff688040ea001b59318953eb805e
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2023-11-26 13:18:57 +0900

    Sign Up Form Challenge
    Sign Up Form Challenge

--- lib/screens/features/authentication/login_screen.dart
+++ lib/screens/features/authentication/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/screens/features/authentication/widgets/auth_button.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -42,11 +43,13 @@ class LoginScreen extends StatelessWidget {
               AuthButton(
                 icon: FaIcon(FontAwesomeIcons.user),
                 text: "Use email & password",
+                destination: EmailScreen(),
               ),
               Gaps.v16,
               AuthButton(
                 icon: FaIcon(FontAwesomeIcons.apple),
                 text: "Continue with Apple",
+                destination: EmailScreen(),
               ),
             ],
           ),
--- lib/screens/features/authentication/widgets/auth_button.dart
+++ lib/screens/features/authentication/widgets/auth_button.dart
@@ -1,46 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class AuthButton extends StatelessWidget {
   final FaIcon icon;
   final String text;
+  final Widget destination;
 
   const AuthButton({
     super.key,
     required this.icon,
     required this.text,
+    required this.destination,
   });
+
+  _onTap(BuildContext context, Widget destination) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => destination),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: Container(
-        padding: const EdgeInsets.all(Sizes.size14),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: const Color.fromRGBO(238, 238, 238, 1),
-            width: Sizes.size1,
+    return GestureDetector(
+      onTap: () => _onTap(context, destination),
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: Container(
+          padding: const EdgeInsets.all(Sizes.size14),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: const Color.fromRGBO(238, 238, 238, 1),
+              width: Sizes.size1,
+            ),
           ),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: icon,
-            ),
-            Text(
-              text,
-              style: const TextStyle(
-                fontSize: Sizes.size16,
-                fontWeight: FontWeight.w600,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: icon,
               ),
-              textAlign: TextAlign.center,
-            ),
-          ],
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
