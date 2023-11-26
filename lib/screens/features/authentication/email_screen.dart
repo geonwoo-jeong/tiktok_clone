fd5750418245444322da3f9234029f66ae9154ee
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2023-11-26 21:32:41 +0900

    Form Button

--- lib/screens/features/authentication/email_screen.dart
+++ lib/screens/features/authentication/email_screen.dart
@@ -1,41 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/features/authentication/widgets/form_button.dart';
 
-class EmailScreen extends StatelessWidget {
+class EmailScreen extends StatefulWidget {
   const EmailScreen({super.key});
+
+  @override
+  State<EmailScreen> createState() => _UsernameScreenState();
+}
+
+class _UsernameScreenState extends State<EmailScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+
+  String _username = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Sign up"),
+      appBar: AppBar(
+        title: const Text("Sign up"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
         ),
-        body: const Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: Sizes.size36,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Gaps.v40,
-              Text(
-                "Create username",
-                style: TextStyle(
-                  fontSize: Sizes.size20,
-                  fontWeight: FontWeight.w700,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              "What is your email?",
+              style: TextStyle(
+                fontSize: Sizes.size20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: "Email",
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade400),
                 ),
               ),
-              Gaps.v8,
-              Text(
-                "You can alway change this later.",
-                style: TextStyle(
-                  fontSize: Sizes.size16,
-                  color: Colors.black54,
-                ),
-              )
-            ],
-          ),
-        ));
+              cursorColor: Theme.of(context).colorScheme.primary,
+            ),
+            Gaps.v16,
+            FormButton(disabled: _username.isEmpty)
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/features/authentication/username_screen.dart
+++ lib/screens/features/authentication/username_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/features/authentication/email_screen.dart';
+import 'package:tiktok_clone/screens/features/authentication/widgets/form_button.dart';
 
 class UsernameScreen extends StatefulWidget {
   const UsernameScreen({super.key});
@@ -23,6 +25,22 @@ class _UsernameScreenState extends State<UsernameScreen> {
         _username = _usernameController.text;
       });
     });
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
+
+  void _onNextTap() {
+    if (_username.isEmpty) return;
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => EmailScreen(),
+      ),
+    );
   }
 
   @override
@@ -71,27 +89,9 @@ class _UsernameScreenState extends State<UsernameScreen> {
               cursorColor: Theme.of(context).colorScheme.primary,
             ),
             Gaps.v16,
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: AnimatedContainer(
-                padding: const EdgeInsets.symmetric(vertical: Sizes.size16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(Sizes.size5),
-                  color: _username.isEmpty
-                      ? Colors.grey.shade300
-                      : Theme.of(context).colorScheme.primary,
-                ),
-                duration: const Duration(milliseconds: 300),
-                child: const Text(
-                  "Next",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            )
+            GestureDetector(
+                onTap: _onNextTap,
+                child: FormButton(disabled: _username.isEmpty))
           ],
         ),
       ),
